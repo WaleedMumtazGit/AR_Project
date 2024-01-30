a2215e5189aeba0bab8aa2abf4925d8642bf4b11
WaleedMumtazGit <waleedmumtazahmad@gmail.com>
2024-01-30 10:03:07 +0500

    test ads working.

--- Assets/_Main/Scripts/ProjectManager.cs
+++ Assets/_Main/Scripts/ProjectManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ProjectManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ProjectManager instance;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
+    IEnumerator Start()
+    {
+        MyAdsManager.instance.InitializeAds();
+       
+
+
+        yield return new WaitForSeconds(10f);
+        MyAdsManager.instance.ShowInterstitialAds();
+        MyAdsManager.instance.ShowAdmobBanner();
+    }
+
     void Update()
     {
-        
+       
     }
 }
